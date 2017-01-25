6471710e86424212b0844d4e7ec70a32fc7c076a
Kel Byers <kel_byers@homedepot.com>
2017-01-24 20:23:34 -0600

    ex2-3

--- julia.py
+++ julia.py
@@ -42,3 +42,16 @@ def calc_pure_python(desired_width, max_iterations):
     # it catches minor errors we might introduce when we're
     # working on a fixed set of inputs
     assert sum(output) == 33219980
+
+def calculate_z_serial_purepython(maxiter, zs, cs):
+    """Calculate output list using Julia update rule"""
+    output = [0] * len(zs)
+    for i in range(len(zs)):
+        n = 0
+        z = zs[i]
+        c = cs[i]
+        while abs(z) < 2 and n < maxiter:
+            z = z * z + c
+            n += 1
+        output[i] = n
+    return output
