8239754559dc41173eea63df488782b8bff27fbe
Kel Byers <kel_byers@homedepot.com>
2017-01-24 20:02:56 -0600

    ex2-1

---
+++ julia.py
@@ -0,0 +1,6 @@
+'''Julia set generator without optional PIL-based image drawing'''
+import time
+
+# area of complex space to investigate
+x1, x2, y1, y2 = -1.8, 1.8, -1.8, 1.8
+c_real, c_imag = -0.62772, -.42193
